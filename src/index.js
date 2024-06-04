0dd7491d1d7ec4fd20585881e546e8d94b00274c
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2024-06-04 16:18:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <BrowserRouter basename="/signup-signin-redux">
             <ChakraProvider>
               <App />
             </ChakraProvider>
